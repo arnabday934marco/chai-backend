f562b85507800f9fc956bee07c8153d40e822a41
Arnab Day <arnabday934@gmail.com>
2026-07-23 13:00:11 +0600

    added index.js

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// require ('dotenv').config({path: './env'})
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("Error", (error) => {
+            console.log("Application express cannot properly connect to the database")
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error
+    }
+})()
+    */
